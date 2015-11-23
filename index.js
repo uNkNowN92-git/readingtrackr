42c7453b685c4b526e7605408657587bde268591
Christian John Sumampong <uNkNowN92-git@users.noreply.github.com>
2015-11-23 09:47:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
 var http = require("http");
 var express = require('express');
 var path = require('path');
@@ -13,22 +16,9 @@ app.get('/pouch-todo', function(req, res){
     res.sendFile(path.join(__dirname + '/public/pouch-todo/index.html'));
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(server_port, server_ip_address, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-// http.createServer(function (request, response) {
-
-//    // Send the HTTP header 
-//    // HTTP Status: 200 : OK
-//    // Content Type: text/plain
-//    response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-//    // Send the response body as "Hello World"
-//    response.end('Hello World\n');
-// }).listen(8081);
-
-// // Console will print the message
-// console.log('Server running at http://127.0.0.1:8081/');
