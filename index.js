71c6918934cc4e2fc33046f42f97ddc3d0413d7b
uNkNowN92 <christianjohnsumampong@gmail.com>
2015-11-27 19:08:29 +0800

    Change ip address to localhost

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var server_port       = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP   || '10.230.57.17'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP   || 'localhost'
  
 var http    = require("http");
 var express = require('express');
